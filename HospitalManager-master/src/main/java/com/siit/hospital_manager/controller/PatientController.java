312fb835042c38a0d4126aaa9ac2c6bd2554901d
Ionut0208 <114175717+Ionut0208@users.noreply.github.com>
2023-02-11 17:18:30 +0200

    Add files via upload

---
+++ HospitalManager-master/src/main/java/com/siit/hospital_manager/controller/PatientController.java
@@ -0,0 +1,40 @@
+package com.siit.hospital_manager.controller;
+
+import com.siit.hospital_manager.model.dto.*;
+import com.siit.hospital_manager.service.PatientService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public List<PatientDto> findAll(){
+        return patientService.findAll();
+    }
+
+    @PostMapping
+    public void createPatient(@RequestBody @Valid CreatePatientDto createPatientDto){
+        patientService.createPatient(createPatientDto);
+    }
+
+    @GetMapping("{id}")
+    public PatientDto findById(@PathVariable("id") Integer id) {
+        return patientService.findById(id);
+    }
+
+    @PatchMapping
+    public void updatePatient(@RequestBody UpdatePatientDto updatePatientDto){
+        patientService.updatePatient(updatePatientDto);
+    }
+
+}
